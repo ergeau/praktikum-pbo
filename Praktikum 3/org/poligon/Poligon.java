93378c0dde821434dda92cc48d424f5227c27e6e
Raihan Gilang F <100418564+ergeau@users.noreply.github.com>
2023-03-08 17:24:14 +0700

    Add files via upload

---
+++ Praktikum 3/org/poligon/Poligon.java
@@ -0,0 +1,16 @@
+/**
+File : Poligon.java 08/03/2023
+Penulis : Raihan Gilang Firdausy
+NIM : 24060121130065
+Deskripsi : representasi dasar dari objek poligon (segi banyak)
+**/
+
+package org.poligon;
+
+public class Poligon{
+	protected int jumlahSisi;
+	
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+}
