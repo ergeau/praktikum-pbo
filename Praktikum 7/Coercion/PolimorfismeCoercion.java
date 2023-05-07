e8ad615204bd2a5b846f7dedc3f1fcf624a7f634
Raihan Gilang F <100418564+ergeau@users.noreply.github.com>
2023-05-07 18:10:28 +0700

    Add files via upload

---
+++ Praktikum 7/Coercion/PolimorfismeCoercion.java
@@ -0,0 +1,25 @@
+/**
+* File: PolimorfismeCoercion.java 03/05/2023
+* Penulis: Raihan Gilang Firdausy
+* Deskripsi : Program untuk mendemokan Polimorfisme(Coercion)
+*/
+
+public class PolimorfismeCoercion {
+	
+	public static int kuadrat(int bilangan) {
+		return bilangan*bilangan;
+	}
+	
+	public static void main(String[] args) {
+	//deklarasi objek integer
+	Integer bilangan = 10;
+	
+	//objek integer 'dipaksa" untuk diubah
+	//menjadi primitif
+	int hasilKuadrat = kuadrat(bilangan);
+	
+	System.out.printf("Hasil kuadrat %d adalah %d",
+			bilangan, 
+			hasilKuadrat);
+	}
+}
